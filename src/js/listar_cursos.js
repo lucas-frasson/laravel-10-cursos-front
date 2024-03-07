5205450eed7f7d06117395bf770da624a6867aa8
lucas-frasson <lgfrasson36@gmail.com>
2024-03-07 02:33:40 -0300

    Refatoração listar cursos e modal Novo Curso

--- src/js/listar_cursos.js
+++ src/js/listar_cursos.js
@@ -24,38 +24,29 @@ function listarCursos() {
 
       var x = 0;
 
-      var text = "";
-
       while (x < qtd) {
-        var conteudo = msg.data[x];
-        var id = conteudo.id;
-        var nome = conteudo.nome;
-        var plataforma = conteudo.plataforma;
-        var data_inicio = conteudo.data_inicio;
-        var data_fim = conteudo.data_fim;
-        var status = conteudo.status;
+        var { id, nome, plataforma, data_inicio, data_fim, status } = msg.data[x];
 
-        if(data_fim == null) {
-          data_fim = '-';
-        }
+        data_fim = data_fim === null ? '-' : data_fim;
 
-        var status = status == 'c' ? 'Cursando' : 'Finalizado';
+        status = status == 'c' ? 'Cursando' : 'Finalizado';
 
-        text += "<tr class='flex flex-col flex-no wrap sm:table-row mb-5 sm:mb-0 bg-white hover:bg-gray-200'>";
-				text += "<td class='border-grey-light p-3'>"+ nome +"</td>";
-				text +=	"<td class='border-grey-light p-3'>"+ plataforma +"</td>";
-        text +=	"<td class='border-grey-light p-3'>"+ data_inicio +"</td>";
-        text +=	"<td class='border-grey-light p-3'>"+ data_fim +"</td>";
-        text +=	"<td class='border-grey-light p-3'>"+ status +"</p></td>";
-				text +=	"<td class='border-grey-light p-3 flex gap-4 text-xl'>";
-        text +=	"<i class='fa-solid fa-pen-to-square cursor-pointer text-neutral-700 hover:text-neutral-900'></i>";
-        text +=	"<i class='fa-solid fa-trash text-red-600 hover:text-red-700 cursor-pointer'></i>";
-        text +=	"</td>";
-        text += "</tr>";
+        text = `
+        <tr class='flex flex-col flex-no wrap sm:table-row mb-5 sm:mb-0 bg-white hover:bg-gray-200'>
+          <td class='border-grey-light p-3'>${nome}</td>
+          <td class='border-grey-light p-3'>${plataforma}</td>
+          <td class='border-grey-light p-3'>${data_inicio}</td>
+          <td class='border-grey-light p-3'>${data_fim}</td>
+          <td class='border-grey-light p-3'>${status}</td>
+          <td class='border-grey-light p-3 flex gap-4 text-xl'>
+            <i class='fa-solid fa-pen-to-square cursor-pointer text-neutral-700 hover:text-neutral-900'></i>
+            <i class='fa-solid fa-trash text-red-600 hover:text-red-700 cursor-pointer'></i>
+          </td>
+        </tr>`;
         x++;
       }
 
-      document.getElementById("listar_cursos").innerHTML = text;
+      $("#listar_cursos").html(text);
     })
     .fail(function (jqXHR, textStatus, msg) {
 
@@ -63,7 +54,7 @@ function listarCursos() {
 
       var text = "Nenhum registro consultado...";
 
-      document.getElementById("listar_cursos").innerHTML = text;
+      $("#listar_cursos").html(text);
 
       console.log('Erro ao listar cursos');
     })
